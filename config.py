e48a18d0407c26c5856c72330d0304ad9c5e4677
AnimeShadow2024 <147716527+AnimeShadow2024@users.noreply.github.com>
2023-10-12 20:42:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8040")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Filesharingbot:filesharingbot@cluster0.eqmncmi.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "Filesharingbot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001575992229"))
